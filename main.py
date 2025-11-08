3d95b92994beddcfa5c92a0d35021ce3d7ebee71
Adityya Kaushal <adityyakaushal@gmail.com>
2025-11-08 03:51:58 -0500

    feat: hand and person tracking

--- main.py
+++ main.py
@@ -1,107 +1,43 @@
-import cv2
-
+import cv2 
 from ultralytics import YOLO
-import cv2
 import math
+import mediapipe as mp
 
-# start webcam
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+stream = cv2.VideoCapture(0)
+stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+stream.set(3, 640)
+stream.set(4, 480)
 
-# model
 model = YOLO("yolo11m.pt")
 
-# object classes
-classNames = [
-    "person",
-    "bicycle",
-    "car",
-    "motorbike",
-    "airplane",
-    "bus",
-    "train",
-    "truck",
-    "boat",
-    "traffic light",
-    "fire hydrant",
-    "stop sign",
-    "parking meter",
-    "bench",
-    "bird",
-    "cat",
-    "dog",
-    "horse",
-    "sheep",
-    "cow",
-    "elephant",
-    "bear",
-    "zebra",
-    "giraffe",
-    "backpack",
-    "umbrella",
-    "handbag",
-    "tie",
-    "suitcase",
-    "frisbee",
-    "skis",
-    "snowboard",
-    "sports ball",
-    "kite",
-    "baseball bat",
-    "baseball glove",
-    "skateboard",
-    "surfboard",
-    "tennis racket",
-    "bottle",
-    "wine glass",
-    "cup",
-    "fork",
-    "knife",
-    "spoon",
-    "bowl",
-    "banana",
-    "apple",
-    "sandwich",
-    "orange",
-    "broccoli",
-    "carrot",
-    "hot dog",
-    "pizza",
-    "donut",
-    "cake",
-    "chair",
-    "sofa",
-    "pottedplant",
-    "bed",
-    "diningtable",
-    "toilet",
-    "tvmonitor",
-    "laptop",
-    "mouse",
-    "remote",
-    "keyboard",
-    "cell phone",
-    "microwave",
-    "oven",
-    "toaster",
-    "sink",
-    "refrigerator",
-    "book",
-    "clock",
-    "vase",
-    "scissors",
-    "teddy bear",
-    "hair drier",
-    "toothbrush",
-]
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
+if not stream.isOpened():
+    print("No Stream :(")
+    exit() 
 
-while True:
-    success, img = cap.read()
-    results = model(img, stream=True)
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
-    # coordinates
+while (True):
+    ret, frame = stream.read()
+    if not ret:
+        print("No more stream")
+        exit()
+
+    results = model(frame, stream=True)
+
     for r in results:
         boxes = r.boxes
 
@@ -110,37 +46,59 @@ while True:
             cls = int(box.cls[0])
             print("Class name -->", classNames[cls])
 
-            if classNames[cls] == "person":
-                # bounding box
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = (
-                    int(x1),
-                    int(y1),
-                    int(x2),
-                    int(y2),
-                )  # convert to int values
+            if classNames[cls] != "person":
+                continue
 
-                # put box in cam
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
-                # confidence
-                confidence = math.ceil((box.conf[0] * 100)) / 100
-                print("Confidence --->", confidence)
+            # put box in cam
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-                # object details
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
 
-                cv2.putText(
-                    img, classNames[cls], org, font, fontScale, color, thickness
-                )
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
 
-    cv2.imshow("Webcam", img)
-    if cv2.waitKey(1) == ord("q"):
-        break
+            cv2.putText(frame, classNames[cls], org, font, fontScale, color, thickness)
 
-cap.release()
+    with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+        if not ret:
+            print("Ignoring empty camera frame.")
+        # If loading a video, use 'break' instead of 'continue'.
+            continue
+
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        frame.flags.writeable = False
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame)
+
+        # Draw the hand annotations on the image.
+        frame.flags.writeable = True
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+    cv2.imshow("Webcam", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break 
+
+stream.release()
 cv2.destroyAllWindows()
